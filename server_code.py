1c4c9571594e78c20b8abd32e6b31edf927714b2
rightexe <146052678+rightexe@users.noreply.github.com>
2023-09-27 11:12:47 +0530

    change in server code

--- server_code.py
+++ server_code.py
@@ -1,29 +1,22 @@
-import asyncio
-import websockets
+from fastapi import FastAPI, WebSocket
+from typing import Set
 
-# Maintain a list of connected clients
-clients = set()
+app = FastAPI()
 
-async def server(websocket, path):
-    # Add the connected client to the list
-    clients.add(websocket)
-    try:
-        async for message in websocket:
-            # Broadcast the received message to all connected clients
-            for client in clients:
-                await client.send(message)
-    except websockets.exceptions.ConnectionClosedError:
-        pass
-    finally:
-        # Remove the client when they disconnect
-        clients.remove(websocket)
+# Maintain a set of connected WebSocket clients
+connected_clients: Set[WebSocket] = set()
 
-if __name__ == "__main__":
-    # Replace '0.0.0.0' with your server's IP address or hostname
-    server_host = '0.0.0.0'
-    server_port = 8765
 
-    start_server = websockets.serve(server, server_host, server_port)
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+  await websocket.accept()
+  connected_clients.add(websocket)
 
-    asyncio.get_event_loop().run_until_complete(start_server)
-    asyncio.get_event_loop().run_forever()
+  try:
+    while True:
+      message = await websocket.receive_text()
+      # Broadcast the received message to all connected clients
+      for client in connected_clients:
+        await client.send_text(message)
+  except WebSocketDisconnect:
+    connected_clients.remove(websocket)
